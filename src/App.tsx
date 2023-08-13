a8afadce26b5a85078ee06c438e136b134f93c7e
Vertori <115083330+Vertori@users.noreply.github.com>
2023-08-13 10:21:03 +0200

    app setup, website scheme /w routes

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProductDetails from "./pages/ProductDetails";
+import WomensClothing from "./pages/WomensClothing";
+import MensClothing from "./pages/MensClothing";
+import Jewelery from "./pages/Jewelery";
+import Home from "./pages/Home";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
+
+function App() {
+
+  return (
+    <div className="overflow-hidden">
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/women" element={<WomensClothing />} />
+        <Route path="/men" element={<MensClothing />} />
+        <Route path="/jewelery" element={<Jewelery />} />
+      </Routes>
+      <Sidebar />
+      <Footer />
+    </Router>
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/CategorizedHero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategorizedHero = () => {
+  return (
+    <div>CategorizedHero</div>
+  )
+}
+
+export default CategorizedHero
