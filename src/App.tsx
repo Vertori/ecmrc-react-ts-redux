f495534093ee81727d5b7aac9c7b1a292772b933
Vertori <115083330+Vertori@users.noreply.github.com>
2023-08-15 10:42:36 +0200

    scroll restoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ProductDetails from "./pages/ProductDetails";
@@ -8,26 +8,26 @@ import Jewelery from "./pages/Jewelery";
 import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
-
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-
   return (
     <div className="overflow-hidden">
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/women" element={<WomensClothing />} />
-        <Route path="/men" element={<MensClothing />} />
-        <Route path="/jewelery" element={<Jewelery />} />
-      </Routes>
-      <Sidebar />
-      <Footer />
-    </Router>
-  </div>
-  )
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/women" element={<WomensClothing />} />
+          <Route path="/men" element={<MensClothing />} />
+          <Route path="/jewelery" element={<Jewelery />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
